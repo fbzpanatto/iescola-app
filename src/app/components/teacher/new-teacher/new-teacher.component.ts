78b50447553235646a3b5a87434e9257371596d8
Fabrizio Panato <fbzpanatto@hotmail.com>
2022-10-06 17:22:56 -0300

    staging

--- src/app/components/teacher/new-teacher/new-teacher.component.ts
+++ src/app/components/teacher/new-teacher/new-teacher.component.ts
@@ -3,13 +3,16 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-new-teacher',
   templateUrl: './new-teacher.component.html',
-  styleUrls: ['./new-teacher.component.scss']
+  styleUrls: ['../home-teacher/home-teacher.component.scss']
 })
 export class NewTeacherComponent implements OnInit {
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onSave(){
   }
 
 }
