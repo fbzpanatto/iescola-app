feb2579daf8d94fda2dae7e9e77337653aa88de7
Fabrizio Panato <fbzpanatto@hotmail.com>
2022-10-06 16:39:10 -0300

    staging

--- src/app/components/teacher/teacher-routing.module.ts
+++ src/app/components/teacher/teacher-routing.module.ts
@@ -2,15 +2,20 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeTeacherComponent } from "./home-teacher/home-teacher.component";
 import { NewTeacherComponent } from "./new-teacher/new-teacher.component";
+import {DetailTeacherComponent} from "./detail-teacher/detail-teacher.component";
 
 const routes: Routes = [
   {
-    path: "",
+    path: '',
     component: HomeTeacherComponent
   },
   {
-    path: "new",
+    path: 'new',
     component: NewTeacherComponent
+  },
+  {
+    path: ':id',
+    component: DetailTeacherComponent
   }
 ];
 
