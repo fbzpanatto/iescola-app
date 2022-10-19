982a96bb2a70c7382bfe9912fcaaa97e0885e2ef
Fabrizio Panato <fbzpanatto@hotmail.com>
2022-10-19 17:04:26 -0300

    staging

--- src/app/shared/components/toolbars/home-toolbar/home-toolbar.component.ts
+++ src/app/shared/components/toolbars/home-toolbar/home-toolbar.component.ts
@@ -10,6 +10,7 @@ import {ActivatedRoute, Router} from "@angular/router";
 export class HomeToolbarComponent implements OnInit {
 
   control: boolean | undefined = true
+  private id: string | null = null
   @Input() title: string | undefined
   @Input() firstPhrase: string | undefined
   @Input() secondPhrase: string | undefined
@@ -19,12 +20,14 @@ export class HomeToolbarComponent implements OnInit {
   @Input() changeViewOptions: boolean = false
   @Input() backButton: boolean = false
   @Input() backLevels: number = 1
+  @Input() useHistory: boolean = false
 
   constructor(private router: Router, private route: ActivatedRoute) {
   }
 
   ngOnInit(): void {
     HomeToolbarService.grid = true
+    this.route.paramMap.subscribe(params => this.id = params.get('id'))
   }
 
   changeView(){
@@ -41,8 +44,15 @@ export class HomeToolbarComponent implements OnInit {
     this.router.navigate(["new"], {relativeTo: this.route})
   }
 
+  goEdit() {
+    this.router.navigate(["../"], {relativeTo: this.route})
+  }
+
   goBack(backLevels: number) {
-    this.router.navigate(["../".repeat(backLevels)], {relativeTo: this.route})
+    if(!this.useHistory) {
+      this.router.navigate(["../".repeat(backLevels)], {relativeTo: this.route})
+    }
+    history.back()
   }
 }
 
