77d6f919cabc5484c76c6ff1c145634e8dfd285c
Fabrizio Panato <fbzpanatto@hotmail.com>
2022-10-20 12:29:44 -0300

    staging

--- src/app/shared/components/toolbars/toolbars.module.ts
+++ src/app/shared/components/toolbars/toolbars.module.ts
@@ -1,25 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeToolbarComponent } from './home-toolbar/home-toolbar.component';
-import { NewToolbarComponent } from './new-toolbar/new-toolbar.component';
 import { MaterialModule } from "../../modules/material.module";
-import { FlexLayoutModule } from "@angular/flex-layout";
-import { MatRippleModule } from "@angular/material/core";
 
 @NgModule({
   declarations: [
     HomeToolbarComponent,
-    NewToolbarComponent,
   ],
     imports: [
         CommonModule,
         MaterialModule,
-        FlexLayoutModule,
-        MatRippleModule
     ],
     exports: [
         HomeToolbarComponent,
-        NewToolbarComponent,
     ]
 })
 export class ToolbarsModule { }
