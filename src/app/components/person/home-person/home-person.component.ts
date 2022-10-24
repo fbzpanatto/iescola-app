a8314280afab31fe73b454f3806d60272d5c248c
Fabrizio Panato <fbzpanatto@hotmail.com>
2022-10-24 09:33:24 -0300

    staging

--- src/app/components/person/home-person/home-person.component.ts
+++ src/app/components/person/home-person/home-person.component.ts
@@ -1,27 +1,41 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit} from '@angular/core';
 import { Title } from "@angular/platform-browser";
 import { HomeToolbarService } from "../../../shared/components/toolbars/service/home-toolbar.service";
 import { PersonService } from "../person.service";
 import { person } from "../person";
+import { Subscription } from "rxjs";
 
 @Component({
   selector: 'app-home-person',
   templateUrl: './home-person.component.html',
   styleUrls: ['./home-person.component.scss']
 })
-export class HomePersonComponent implements OnInit {
+export class HomePersonComponent implements OnInit, OnDestroy {
 
   title: string | undefined
   listView: boolean | undefined = true
   persons: person[] = []
+  subscription: Subscription = new Subscription()
 
   constructor(private titleService: Title, private personService: PersonService) { }
 
-  ngOnInit(): void {
-    this.title = this.titleService.getTitle().split('-')[1]
-    HomeToolbarService.subject.subscribe(v => this.listView = v)
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe()
+  }
 
-    this.personService.getAll().subscribe(console.log)
+  ngOnInit(): void {
+    this.onLoad()
+      .then(() => {
+        this.subscription = this.personService.getAll().subscribe(persons => this.persons = persons)
+      })
+  }
+
+  onLoad() {
+    return new Promise<void>((resolve) => {
+      this.title = this.titleService.getTitle().split('-')[1]
+      HomeToolbarService.subject.subscribe(v => this.listView = v)
+      resolve()
+    })
   }
 
   onEdit(){
