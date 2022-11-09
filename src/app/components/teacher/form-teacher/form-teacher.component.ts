48e1760e496ad392cd207a96ab13b9142e196d86
Fabrizio Panato <fbzpanatto@hotmail.com>
2022-11-09 09:14:42 -0300

    staging

--- src/app/components/teacher/form-teacher/form-teacher.component.ts
+++ src/app/components/teacher/form-teacher/form-teacher.component.ts
@@ -48,8 +48,12 @@ export class FormTeacherComponent implements OnInit {
     private router: Router
   ) {
     this.filteredDisciplines = this.disciplineControl.valueChanges.pipe(
-      startWith(null),
+      startWith(''),
       map(disciplineName => this.filterOnValueChange(disciplineName))
+    )
+    this.filteredClasses = this.classControl.valueChanges.pipe(
+      startWith(''),
+      map(classroomName => this.filterOnClassValueChange(classroomName))
     )
   }
 
@@ -85,7 +89,17 @@ export class FormTeacherComponent implements OnInit {
         this.httpClient.get(`${environment.GIGABASE.ODATA_URL}/Escola/Class`)
           .subscribe({
             next: (result:any) => {
-              this.allClasses = result.value as clasroom[]
+              // this.allClasses = result.value as clasroom[]
+              this.allClasses = [
+                {
+                  "id": 1,
+                  "name": "1A"
+                },{
+                  "id": 2,
+                  "name": "1B"
+                }
+
+              ]
               resolve()
             },
             error: (err) => reject(err)
@@ -104,9 +118,88 @@ export class FormTeacherComponent implements OnInit {
   }
 
   //ClassChips
+  public addClass(event: MatChipInputEvent): void {
+    if (!this.allowFreeTextAddClass) return
+    if (this.matAutocompleteClass.isOpen) return
 
+    const value = event.value;
+    if ((value || '').trim()) {
+      this.selectClassByName(value.trim());
+    }
 
-  //DisciplineChips
+    this.resetClassInputs();
+  }
+  public removeClass(classroom: clasroom): void {
+    const index = this.chipSelectedClasses.indexOf(classroom);
+    if (index >= 0) {
+      this.chipSelectedClasses.splice(index, 1);
+      this.resetClassInputs();
+    }
+  }
+  public classSelected(event: MatAutocompleteSelectedEvent): void {
+    this.selectClassByName(event.option.value);
+    this.resetClassInputs();
+  }
+  private resetClassInputs() {
+    // clear input element
+    this.classInput.nativeElement.value = '';
+    // clear control value and trigger engineerControl.valueChanges event
+    this.classControl.setValue(null);
+  }
+  filterOnClassValueChange(className: string | null): String[] {
+    let result: String[]
+
+    let allClassesLessSelected = this.allClasses.filter(classroom => this.chipSelectedClasses.indexOf(classroom) < 0)
+    if(className) {
+      result = this.filterClass(allClassesLessSelected, className)
+    } else {
+      result = allClassesLessSelected.map(classroom => classroom.name)
+    }
+    return result
+  }
+  filterClass(classList: clasroom[], className: String): String[] {
+    let filteredClassList: clasroom[]
+    const filterValue = className.toLowerCase();
+    let classesMatchingClassName = classList.filter(classroom => classroom.name.toLowerCase().indexOf(filterValue) === 0);
+    if (classesMatchingClassName.length || this.allowFreeTextAddClass) {
+      //
+      // either the engineer name matched some autocomplete options
+      // or the name didn't match but we're allowing
+      // non-autocomplete engineer names to be entered
+      //
+      filteredClassList = classesMatchingClassName;
+    } else {
+      //
+      // the engineer name didn't match the autocomplete list
+      // and we're only allowing engineers to be selected from the list
+      // so we show the whjole list
+      //
+      filteredClassList = classList;
+    }
+    //
+    // Convert filtered list of engineer objects to list of engineer
+    // name strings and return it
+    //
+    return filteredClassList.map(classroom => classroom.name);
+  }
+  private selectClassByName(className: string) {
+    let foundClass = this.allClasses.filter(classroom => classroom.name == className);
+    if (foundClass.length) {
+      //
+      // We found the engineer name in the allEngineers list
+      //
+      this.chipSelectedClasses.push(foundClass[0]);
+    } else {
+      //
+      // Create a new engineer, assigning a new higher employeeId
+      // This is the use case when allowFreeTextAddEngineer is true
+      //
+      let highestClassId = Math.max(...this.chipSelectedClasses.map(clasroom => clasroom.id), 0);
+      this.chipSelectedClasses.push({ name: className, id: highestClassId + 1 });
+    }
+  }
+
+  // DisciplineChips
   public addDiscipline(event: MatChipInputEvent): void {
     if (!this.allowFreeTextAddDiscipline) {
       // only allowed to select from the filtered autocomplete list
@@ -128,27 +221,27 @@ export class FormTeacherComponent implements OnInit {
       this.selectDisciplineByName(value.trim());
     }
 
-    this.resetInputs();
+    this.resetDisciplineInputs();
   }
   public removeDiscipline(discipline: discipline): void {
     const index = this.chipSelectedDisciplines.indexOf(discipline);
     if (index >= 0) {
       this.chipSelectedDisciplines.splice(index, 1);
-      this.resetInputs();
+      this.resetDisciplineInputs();
     }
   }
   public disciplineSelected(event: MatAutocompleteSelectedEvent): void {
     this.selectDisciplineByName(event.option.value);
-    this.resetInputs();
+    this.resetDisciplineInputs();
   }
-  private resetInputs() {
+  private resetDisciplineInputs() {
     // clear input element
     this.disciplineInput.nativeElement.value = '';
     // clear control value and trigger engineerControl.valueChanges event
     this.disciplineControl.setValue(null);
   }
   filterOnValueChange(disciplineName: string | null): String[] {
-    let result: String[] = []
+    let result: String[]
 
     let allDisciplinesLessSelected = this.allDisciplines.filter(discipline => this.chipSelectedDisciplines.indexOf(discipline) < 0)
     if(disciplineName) {
@@ -159,7 +252,7 @@ export class FormTeacherComponent implements OnInit {
     return result
   }
   filterDiscipline(disciplineList: discipline[], disciplineName: String): String[] {
-    let filteredDisciplineList: discipline[] = [];
+    let filteredDisciplineList: discipline[]
     const filterValue = disciplineName.toLowerCase();
     let disciplinesMatchingDisciplineName = disciplineList.filter(discipline => discipline.name.toLowerCase().indexOf(filterValue) === 0);
     if (disciplinesMatchingDisciplineName.length || this.allowFreeTextAddDiscipline) {
